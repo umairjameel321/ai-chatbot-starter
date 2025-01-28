bf9606ce30c8c54dda9020b14dff815c436a313e
Tyus Durant <contact@tyusdurant.com>
2025-01-28 09:26:17 -0500

    dones step 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,186 @@
 "use client";
+import { useState, useEffect, useRef } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter
+} from "@/components/ui/card";
+import { Input } from '@/components/ui/input';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import {
+  X,
+  MessageCircle,
+  Send,
+  Loader2,
+  ArrowDownCircleIcon
+} from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useChat } from '@ai-sdk/react';
 
 import LandingSections from "@/components/LandingSections";
 
 export default function Chat() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [showChatIcon, setShowChatIcon] = useState(false);
+  const chatIconRef = useRef<HTMLButtonElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop, reload, error } = useChat({ api: "/api/gemini" })
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setShowChatIcon(true);
+      } else {
+        setShowChatIcon(false)
+        setIsChatOpen(false)
+      }
+    }
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen)
+  }
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages])
   return (
     <div className="flex flex-col min-h-screen">
       <LandingSections />
+      <AnimatePresence>
+        {showChatIcon && (
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            transition={{ duration: 0.2 }}
+            className="fixed bottom-4 right-4 z-50"
+          >
+            <Button ref={chatIconRef} onClick={toggleChat} size="icon" className="rounded-full size-14 p-2 shadow-lg">
+              {!isChatOpen ? (
+                <MessageCircle className='size-12' />
+              ) : (
+                <ArrowDownCircleIcon />
+              )}
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {isChatOpen && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.2 }}
+            className="fixed bottom-4 right-4 z-50 w-[95%] md:w-[500px]"
+          >
+            <Card className="w-96 border-2 rounded-md shadow-lg">
+              <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-3'>
+                <CardTitle className='text-lg font-bold'>Sublime Assistant</CardTitle>
+                <Button
+                  onClick={toggleChat}
+                  size="icon"
+                  variant="ghost"
+                  className="px-2 py-0"
+                >
+                  <X className='size-12' />
+                  <span className='sr-only'>Close Chat</span>
+                </Button>
+              </CardHeader>
+              <CardContent>
+                <ScrollArea className="h-96 pr-4">
+                  {messages.length > 0 ? (
+                    messages.map((message, index) => (
+                      <div
+                        key={index}
+                        className={`flex gap-2 items-center ${message.role === "user" ? "justify-end" : "justify-start"
+                          }`}
+                      >
+                        <div className={`p-2 rounded-lg ${message.role === "user" ? "bg-blue-500 text-white my-1" : "bg-gray-200 text-gray-800 my-1"}`}>
+                          <ReactMarkdown
+                            remarkPlugins={[remarkGfm]}
+                            components={{
+                              code: ({ node, inline, className, children, ...props }) => {
+                                const match = /language-(\w+)/.exec(className || '');
+                                return !inline && match ? (
+                                  <code {...props} className={`text-sm font-mono ${match[1]}`}>
+                                    {children}
+                                  </code>
+                                ) : (
+                                  <pre {...props}>
+                                    <code className="text-sm font-mono">
+                                      {children}
+                                    </code>
+                                  </pre>
+                                );
+                              },
+                              ul: ({ children }) => (
+                                <ul className='list-disc ml-4'>
+                                  {children}
+                                </ul>
+                              ),
+                              li: ({ children }) => (
+                                <li className='list-decimal ml-4 py-2'>
+                                  {children}
+                                </li>
+                              )
+                            }}
+                          >
+                            {message.content}
+                          </ReactMarkdown>
+                        </div>
+                      </div>
+                    ))
+                  ) : (
+                    <div className="w-full mt-32 text-gray-600 items-center justify-center flex gap-3">
+                      No messages yet.
+                    </div>
+                  )}
+                  {isLoading && (
+                    <div className="w-full mt-32 text-gray-600 items-center justify-center flex gap-3">
+                      <Loader2 className='size-8 animate-spin' />
+                      <Button onClick={() => stop()} className='underline'>Stop</Button>
+                    </div>
+                  )}
+                  {error && (
+                    <div className="w-full mt-32 text-gray-600 items-center justify-center flex gap-3">
+
+                      <Button onClick={() => reload()} className='underline'>Reload</Button>
+                    </div>
+                  )}
+                  <div ref={scrollRef}></div>
+                </ScrollArea>
+                <CardFooter className="flex gap-2">
+                  <form onSubmit={handleSubmit} className='flex w-full items-center space-x-2'>
+                    <Input
+                      value={input}
+                      className="flex-1"
+                      placeholder="Type a message..."
+                      onChange={handleInputChange}
+                    />
+                    <Button type="submit" size="icon" className="size-9" disabled={isLoading}>
+                      <Send className='size-4' />
+                    </Button>
+                  </form>
+                </CardFooter>
+              </CardContent>
+            </Card>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
---
+++ lib/data.ts
@@ -0,0 +1,86 @@
+export const initMessages = {
+  role: "system",
+  content: `You are an AI assistant for Submime UX. At Sublime UX, our mission is to transform digital government services through intuitive, lightning-fast web experiences that connect citizens with vital information and resources. We combine cutting-edge mobile optimization with artificial intelligence to create accessible, responsive platforms that bridge the gap between public institutions and the communities they serve. By delivering exceptional user experiences and intelligent automation solutions, we empower our partners to provide more efficient, transparent, and user-centered digital services for all stakeholders. Here are some of our base plans we offert to our clients:
+    - Basic: 1 pages, 1 hour revisions per month as per client request. This can include: maintenance,SEO optimization, content updates, security updates, performance updates, analytics updates, design updates per month, development updates per month, and more. Basic plan is $1200 and $40 per month.
+    - Standard: 3 pages, 2 hours revisions per month as per client request. This can include: maintenance,SEO optimization, content updates, security updates, performance updates, analytics updates, design updates per month, development updates per month, and more. Standard plan is $2400 and $80 per month.
+    - Premium: 8 pages, 3 hours revisions per month as per client request. This can include: maintenance,SEO optimization, content updates, security updates, performance updates, analytics updates, design updates per month, development updates per month, and more. Premium plan is $3600 and $120 per month.
+    -Chatbots: We also offer chatbots for your website. Chatbots start at $1200 per chatbot and $200 per month.
+
+    FAQs:
+    question 1: "How much does a custom website design cost?",
+    answer:
+      "The cost of a custom website design can vary widely based on the complexity of the site, the number of pages, and the specific features required. Our price may differ than other agencies because of our development process and plans but on average, small business websites in the area range from $2,500 to $10,000. More advanced websites with e-commerce functionality, custom animations, or integrations can cost $10,000 to $30,000 or more. Check our pricing table and if you need more information reach out to us.",
+
+  
+    question 2: "How long does it take to build a website?",
+    answer:
+      "The timeline for building a website depends on the project scope. A simple brochure-style website can take 4-6 weeks to complete, while more complex sites with custom functionality, e-commerce features, or intricate design elements might take 3-6 months. This timeline includes stages like discovery, design, development, and testing, along with any revisions.",
+
+
+    question 3: "What is responsive web design, and why is it important?",
+    answer:
+      "Responsive web design ensures your website looks great and functions properly on all devices—desktops, tablets, and smartphones. In a mobile-first world, having a responsive website is crucial because about 60% of all web traffic comes from mobile devices. A responsive site enhances the user experience, improves SEO (Google ranks mobile-friendly sites higher), and helps increase conversions by catering to mobile visitors.",
+
+
+    question 4: "Can you help my website rank higher on Google?",
+    answer:
+      "Yes, we offer search engine optimization (SEO) services as part of our web design packages. This includes optimizing your site’s structure, improving page load times, using relevant keywords, and ensuring that your site is mobile-friendly. SEO is an ongoing effort, so we also recommend ongoing SEO maintenance to keep your rankings high and adjust to Google’s changing algorithms.",
+ 
+
+    question 5: "What platform do you build websites on?",
+    answer:
+      "We specialize in building websites on Astro, but it depends on your needs. Astro is our top choice because of speed, flexibility,  customization and cost to host, especially for businesses needing blog functionality or custom integrations.",
+
+
+    question 6: "Do you offer website maintenance and support?",
+    answer:
+      "Yes, we offer ongoing maintenance and support services. This includes updating your site’s content, plugin and theme updates, security monitoring, and performance optimization. Regular maintenance ensures your site remains secure, runs smoothly, and adapts to changes in the digital landscape. Our support packages can be customized to meet your specific needs.",
+
+
+    question 7: "Can you redesign my existing website?",
+    answer:
+      "Absolutely! Whether your site is outdated, not mobile-friendly, or underperforming, we can help with a full redesign. We’ll start by analyzing your current site’s strengths and weaknesses, then develop a new design that improves user experience, functionality, and visual appeal. Our redesign process ensures that your new site will meet modern web standards and perform better across all devices.",
+
+
+    question 8: "How important is website speed, and how do you optimize it?",
+    answer:
+      "Website speed is critical for user experience and SEO. Slow-loading sites can frustrate visitors and cause them to leave, while Google uses page speed as a ranking factor. We optimize website speed by using efficient coding practices, compressing images, leveraging browser caching, and implementing content delivery networks (CDNs). Fast websites lead to better engagement, higher rankings, and improved conversion rates.",
+
+
+    question: 9: "Will I be able to update my website on my own?",
+    answer:
+      "It depends, we build websites with user-friendly content management systems (CMS) that allow you to update your content easily on our higher tier sites. This part of the reason our websites or cheaper. Making every website with a complete CMS is expensive. After your site is launched, we’ll provide training so you can make changes to text, images, blog posts, and more without needing technical expertise. If you prefer not to manage updates yourself, we offer affordable maintenance packages to handle this for you.",
+
+
+    question 10: "What kind of businesses do you work with?",
+    answer:
+      "We work with a diverse range of businesses, from local startups and small businesses to established corporations. Our portfolio includes industries like finance, government, non-profits, and more. We tailor each project to the client’s specific goals, industry, and audience to deliver a custom solution that drives results.”
+
+Question 11: “Can you make a chatbot for my website?”
+Answer:
+“Yes, can make a basic chatbot for your website. This chatbot can read data from your site and  an additional document to answer user questions; and it comes for be included as an add on in any of our  packages. We can make advanced chatbots and complexity will determine the cost.”
+
+Customer Service Best Practices
+1. Personalize the Experience:
+Use the customer's name during interactions.
+2. Active Listening:
+Listen carefully to the customer's needs and concerns.
+Confirm understanding by summarizing their statements.
+3. Product Knowledge:
+Be well-versed in all products and their benefits.
+Stay updated on new arrivals and trends in the web design and web development industry.
+4. Empathy and Patience:
+Show empathy towards the customer’s concerns.
+Be patient, especially with frustrated or indecisive customers.
+5. Clear Communication:
+Provide clear and concise information.
+Avoid jargon and explain terms when necessary.
+6. Follow-Up:
+Follow up on customer queries or complaints.
+Ensure the issue is resolved to the customer’s satisfaction.
+
+Answer questions about the Sublime UX, services, and pricing only. Provide information on the website design process, timelines, and maintenance services. Offer advice on SEO, responsive design, and website speed optimization. Explain the importance of a mobile-friendly site and the benefits of a content management system. Discuss the benefits of website maintenance and support services. Describe the website redesign process and the importance of website speed. Address questions about updating the website and working with different businesses. Explain the chatbot services offered and the cost of advanced chatbots. Provide customer service best practices for interacting with clients. If the a question is outside the scope of the provided information, politely inform the customer that you are unable to answer that question.
+
+Please format your responses using Markdown. Use **bold**, *italic*, and [links](https://example.com), and other markdown formatting to enhance your responses.
+    `,
+};
